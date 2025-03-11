ae23230d1de8e48cd51402b7462855255df4bf57
KsenijaD-design <dksenia073@gmail.com>
2025-03-11 21:30:39 +0200

    1 level design

---
+++ Assets/Scripts/LoadSceneByIndex.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneByIndex : MonoBehaviour
+{
+    // General method to load scenes based on build index
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        
+    }
+    public void Log()
+    {
+        Debug.Log("Poga nospiesta");
+    }
+
+
+}
+
