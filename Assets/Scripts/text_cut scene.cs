44f3ac3180953d9db7eabbcf519a1047d3180a62
KsenijaD-design <dksenia073@gmail.com>
2025-04-08 19:03:25 +0300

    Cut scene with text

---
+++ Assets/Scripts/text_cut scene.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class text_cutscene : MonoBehaviour
+{
+    public GameObject[] textObjects;  // Сюда закидываем все текстовые окна
+    private int currentIndex = 0;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // ЛКМ
+        {
+            if (currentIndex < textObjects.Length)
+            {
+                textObjects[currentIndex].SetActive(true);
+                currentIndex++;
+            }
+        }
+    }
+}
